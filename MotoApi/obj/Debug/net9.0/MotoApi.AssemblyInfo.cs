9c845d2b18a651441b1382040cfd51d34544ff35
RichardyBS <editionplays@gmail.com>
2025-05-25 14:40:49 -0300

    readme.md att

--- MotoApi/obj/Debug/net9.0/MotoApi.AssemblyInfo.cs
+++ MotoApi/obj/Debug/net9.0/MotoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MotoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11509d5e71128504d122b16cc803993a1dffed87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+887cff3596e621a3a5fe5c26973440d2361030f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MotoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MotoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
